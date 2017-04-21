aca0e01b882811e175e5c1162b9924cc33dbaeea
Ethan Barker <ethanbarker0@gmail.com>
2017-04-21 07:01:40 -0700

    Last bit of set-up

--- js/gridManager.js
+++ js/gridManager.js
@@ -1,6 +1,9 @@
 "use strict";
 function addTiles(count) {
-
+    if (checkFull()) {
+        $(".score").text("Game Over");
+        return;
+    }
     for (var i = 0; i < count; i++) {
         do {
             var randX = Math.floor(Math.random() * grid.length);
@@ -11,13 +14,64 @@ function addTiles(count) {
     }
 }
 
-// Key press dete
+function render() {
+    for (var tX = 0; tX < grid.length; tX++) {
+        for (var tY = 0; tY < grid[tX].length; tY++) {
+            $(".b" + String(tX) + String(tY)).text(String(grid[tX][tY].points));
+        }
+    }
+}
+
+function checkFull() {
+    var isFull = true;
+    for (var tX = 0; tX < grid.length; tX++) {
+        for (var tY = 0; tY < grid[tX].length; tY++) {
+            if (!grid[tX][tY].hasValue) {
+                isFull = false;
+            }
+        }
+    }
+    return isFull;
+}
+
+function moveTiles(key) {
+    if (key > 3) {
+        for (var tX = 0; tX < grid.length; tX++) {
+            for (var tY = 0; tY < grid[tX].length; tY++) {
+
+            }
+        }
+    } else {
+        for (var tX = grid.length; tX >= 0; tX++) {
+            for (var tY = grid.length[tX]; tY >= 0; tY++) {
+                $(".b" + String(tX) + String(tY)).text(String(grid[tX][tY].points));
+            }
+        }
+    }
+}
+
+// Key press detection
 $(function(){
     var html = $('html');
     var firstDown = true;
     html.keydown(function(e){
         if (firstDown) {
+            if (e.which === 37) {
+                // Left
+                moveTiles(4);
+            } else if (e.which === 38) {
+                // Up
+                moveTiles(3);
+            } else if (e.which === 39) {
+                // Right
+                moveTiles(2);
+            } else if (e.which === 40) {
+                // Down
+                moveTiles(1);
+            }
             console.log(e.which);
+            addTiles(1);
+            render();
         }
         firstDown = false;
     });
--- js/main.js
+++ js/main.js
@@ -8,20 +8,13 @@ for (var tX = 0; tX < grid.length; tX++) {
     }
 }
 
-addTiles(10);
-(function (){
-    for (var tX = 0; tX < grid.length; tX++) {
-        for (var tY = 0; tY < grid[tX].length; tY++) {
-            if (grid[tX][tY].hasValue) {
-                console.log(grid[tX][tY].x, grid[tX][tY].y);
-            } else {
-                console.log("Empty")
-            }
-        }
-    }
-})();
+addTiles(2);
 
 
+
+$(document).ready(function () {
+    render();
+});
 // var grid2 = grid;
 // grid2[2][2].x = 10;
 // console.log(grid[2][2].x);
